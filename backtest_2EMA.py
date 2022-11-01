ed4936099e1ac55badf2cffb0777bb1b605d81ff
vazw <4wonzest@gmail.com>
2022-11-01 18:18:57 +0700

    update

--- backtest_2EMA.py
+++ backtest_2EMA.py
@@ -23,29 +23,49 @@ exchange = ccxt.binance()
 symboli = SYMBOL_NAME + "/USDT"
 
 
-print(f"Fetching new bars for {dt.now().isoformat()}")
-bars = exchange.fetch_ohlcv(symboli, timeframe=TF, since=None, limit=2002)
+print(f"Backtesting {symboli} on timeframe {TF}")
+bars = exchange.fetch_ohlcv(symboli, timeframe=TF, since=None, limit=1502)
 df = pd.DataFrame(
     bars[:-1], columns=["timestamp", "Open", "High", "Low", "Close", "Volume"]
 )
 df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+df = df.set_index("timestamp")
 
 
-def indicator(emafast, emaslow):
+def Ema_Fast(period):
     global df
-    df["emafast"] = ta.ema(df["Close"], int(emafast))
-    df["emaslow"] = ta.ema(df["Close"], int(emaslow))
-    return df["emafast"], df["emaslow"]
+    df["EMA_fast"] = ta.ema(df.Close, period)
+    return df.EMA_fast
+
+
+def Ema_Slow(period):
+    global df
+    df["EMA_slow"] = ta.ema(df.Close, period)
+    return df.EMA_slow
 
 
 def signalbuy():
-    trade = crossover(pd.Series(df["emafast"]), pd.Series(df["emaslow"]))
-    return trade
+    m = len(df.index)
+    df["buy"] = [False] * m
+    for i in range(3, m):
+        if (
+            df["EMA_fast"][i - 1] > df["EMA_slow"][i - 1]
+            and df["EMA_fast"][i - 2] < df["EMA_slow"][i - 2]
+        ):
+            df["buy"][i] = True
+    return df.buy
 
 
 def signalsell():
-    trade = crossover(pd.Series(df["emaslow"]), pd.Series(df["emafast"]))
-    return trade
+    m = len(df.index)
+    df["sell"] = [False] * m
+    for i in range(3, m):
+        if (
+            df["EMA_fast"][i - 1] < df["EMA_slow"][i - 1]
+            and df["EMA_slow"][i - 2] < df["EMA_fast"][i - 2]
+        ):
+            df["sell"][i] = True
+    return df.sell
 
 
 class run_bot(Strategy):
@@ -55,23 +75,28 @@ class run_bot(Strategy):
     def init(self):
         super().init()
         self.A2 = self.I(
-            indicator,
+            Ema_Fast,
             self.ema_fast,
+        )
+
+        self.A1 = self.I(
+            Ema_Slow,
             self.ema_slow,
         )
-        self.A0 = signalbuy
-        self.A1 = signalsell
+        self.A3 = self.I(signalbuy)
+
+        self.A4 = self.I(signalsell)
 
     def next(self):
-        if self.A0:
+        if self.A3:
             self.position.close()
             self.buy()
-        elif self.A1:
+        if self.A4:
             self.position.close()
             self.sell()
 
 
-bt = Backtest(df, run_bot, cash=100000)
+bt = Backtest(df, run_bot, cash=10000)
 stat = bt.run()
 # stat = bt.optimize(
 # ema_fast = range(1,200,2),
